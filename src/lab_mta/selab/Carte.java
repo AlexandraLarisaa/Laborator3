0be91a7aa2ceedc1347fb189e2fe58f2b4627594
alexandra-oras-selab <larisa.oras@mta.ro>
2020-10-27 18:19:24 +0200

    Adaugare Utilizator, modificare Carte
    Detalii....
    
    Signed-off-by: alexandra-oras-selab <larisa.oras@mta.ro>

--- src/lab_mta/selab/Carte.java
+++ src/lab_mta/selab/Carte.java
@@ -3,18 +3,18 @@ package lab_mta.selab;
 public class Carte {
     private String titlu;
     private String autor;
-    private int anAparitie;
+    private int anPublicatie;
     private int nrPagini;
 
-    public Carte(String titlu, String autor, int anAparitie, int nrPagini) {
+    public Carte(String titlu, String autor, int anPublicatie, int nrPagini) {
         this.titlu = titlu;
         this.autor = autor;
-        this.anAparitie = anAparitie;
+        this.anPublicatie = anPublicatie;
         this.nrPagini = nrPagini;
     }
 
     @Override
     public String toString() {
-        return this.titlu+" "+this.autor+ " " + "  publicată în anul "+ this.anAparitie;
+        return this.titlu+" "+this.autor+ " " + "  publicată în anul "+ this.anPublicatie;
     }
 }
---
+++ src/lab_mta/selab/Utilizator.java
@@ -0,0 +1,12 @@
+package lab_mta.selab;
+
+import java.util.ArrayList;
+
+public class Utilizator {
+    private String nume;
+    private ArrayList<Carte> carti;
+
+    public Utilizator(String nume) {
+        this.nume = nume;
+    }
+}
